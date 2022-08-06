02923b65652398285ee271f500987b73e9b9cf61
Clemens Akens <clebert@me.com>
2022-08-06 10:35:49 +0200

    Rename `ObjectStore` to `PermanentStore` and allow non-object values

--- src/permanent-store.ts
+++ src/permanent-store.ts
@@ -1,16 +1,16 @@
 import {Store} from './store.js';
 
-export interface ObjectStoreInit<TValue extends object> {
+export interface PermanentStoreInit<TValue> {
   readonly storage: Pick<Storage, 'getItem' | 'setItem'>;
   readonly key: string;
-  readonly defaultValue: TValue;
+  readonly defaultValue: NonNullable<TValue>;
 }
 
-export class ObjectStore<TValue extends object> extends Store<TValue> {
+export class PermanentStore<TValue> extends Store<NonNullable<TValue>> {
   readonly #storage: Pick<Storage, 'getItem' | 'setItem'>;
   readonly #key: string;
 
-  constructor(init: ObjectStoreInit<TValue>) {
+  constructor(init: PermanentStoreInit<TValue>) {
     const {storage, key, defaultValue} = init;
     const item = storage.getItem(key);
     const value = item ? JSON.parse(item) : defaultValue;
@@ -21,11 +21,11 @@ export class ObjectStore<TValue extends object> extends Store<TValue> {
     this.#key = key;
   }
 
-  override get value(): TValue {
+  override get value(): NonNullable<TValue> {
     return super.value;
   }
 
-  override set value(value: TValue) {
+  override set value(value: NonNullable<TValue>) {
     this.#storage.setItem(this.#key, JSON.stringify((super.value = value)));
   }
 }
